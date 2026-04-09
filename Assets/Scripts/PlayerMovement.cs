39c2f01d24a6330c374d183bd3843948c8254fe6
arisawa <arisawa.teresa@gmail.com>
2026-04-09 15:37:45 -0700

    added flipping when you turn left/right and the attacking motion

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,42 @@ public class PlayerMovement : MonoBehaviour
     public Animator Animator;
 
     private object priorityDevice = null;
-
-
+    
 
     public void Move(Vector2 direction, object inputDevice)
     {
         SetPriorityInputDevice(direction, inputDevice);
+        if (IsNotUsingPriorityInputDevice(inputDevice))
+        {
+            return;
+        }
         
-        // Animator
+        FaceCorrectDirection(direction);
+        Animate(direction);
         ApplyMovement(direction);
+    }
+
+    private bool IsNotUsingPriorityInputDevice(object inputDevice)
+    {
+        return (inputDevice != priorityDevice);
+    }
+
+    private void FaceCorrectDirection(Vector2 direction)
+    {
+        if (IsNotFacingCorrectDirection(direction))
+        {
+            FlipFacingDirection();
+        }
+    }
+
+    private void FlipFacingDirection()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    private bool IsNotFacingCorrectDirection(Vector2 direction)
+    {
+        return direction.x > 0 && transform.localScale.x < 0 || direction.x < 0 && transform.localScale.x > 0; 
     }
 
     private void SetPriorityInputDevice(Vector2 direction, object inputDevice)
@@ -35,5 +62,11 @@ public class PlayerMovement : MonoBehaviour
     private void ApplyMovement(Vector2 direction)
     {
         RigidBody.linearVelocity = direction * Speed;
+    }
+
+    private void Animate(Vector2 direction)
+    {
+        Animator.SetFloat("Horizontal", Mathf.Abs(direction.x));
+        Animator.SetFloat("Vertical", Mathf.Abs(direction.y));
     }
 }
